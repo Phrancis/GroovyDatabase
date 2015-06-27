e64aa77840e71dca5a860e58e3611efb93bae57f
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2015-06-27 05:06:41 -0400

    Swing input works

--- DatabaseTest.groovy
+++ DatabaseTest.groovy
@@ -2,17 +2,21 @@
  * Created by francisveilleux-gaboury on 6/24/15.
  */
 import groovy.sql.Sql
+import groovy.json.JsonSlurper
 
-def dbUrl      = "jdbc:postgresql://localhost/GroovyTest"
-def dbUser     = "Phrancis"
-def dbPassword = "test"
-def dbDriver   = "org.postgresql.Driver"
+def dbUrl      = 'jdbc:postgresql://localhost/GroovyTest'
+def dbUser     = 'Phrancis'
+def dbPassword = 'test'
+def dbDriver   = 'org.postgresql.Driver'
 
 def sql = Sql.newInstance(dbUrl, dbUser, dbPassword, dbDriver)
 
-println "Sql Instance: " + sql
+println 'Sql Instance: ' + sql
 
-sql.execute """
+sql.execute 'SET SEARCH_PATH TO groovy_test;'
+
+sql.execute '''
+        START TRANSACTION;
         DROP TABLE IF EXISTS test;
         CREATE TABLE test (
             id SERIAL,
@@ -21,25 +25,28 @@ sql.execute """
             decimal NUMERIC,
             datetime TIMESTAMP
             );
-        """
+        COMMIT;'''
 
-def params = ['''');drop table test;--''', 42, 3.14159, 'NOW()']
-
-sql.execute """
+def sqlInsert = { List<Object> params ->
+    sql.execute """
+        START TRANSACTION;
         INSERT INTO test (string, number, decimal, datetime)
             VALUES (?, ?, ?, CAST(? AS TIMESTAMP));
-        """, params
-
-sql.eachRow("SELECT * FROM test;") { row ->
-    println """
-        The row Id is: ${row.id}
-        The string is: ${row.string}
-        The number is: ${row.number}
-        The decimal is: ${row.decimal}
-        The date-time is: ${row.datetime}
-    """
+        COMMIT;""", params
 }
-/*sql.eachRow("SELECT * FROM test;") { row ->
-    print ${row}
-}*/
+
+def params1 = ['''');DROP TABLE test;--''', 42, 3.14159, 'NOW()']
+def params2 = ['Hello, World!', 99999999, 0.1209823098234, '2015-06-25']
+
+sqlInsert(params1)
+sqlInsert(params2)
+
+sql.eachRow('SELECT * FROM test;') { row ->
+    println """
+The row Id is: ${row.id}
+The string is: ${row.string}
+The number is: ${row.number}
+The decimal is: ${row.decimal}
+The date-time is: ${row.datetime}"""
+}
 sql.close()
--- SwingForm.groovy
+++ SwingForm.groovy
@@ -1,7 +1,3 @@
-/**
-*  Created by francisveilleux-gaboury on 6/25/15.
-*/
-
 import groovy.swing.SwingBuilder
 import groovy.beans.Bindable
 import static javax.swing.JFrame.EXIT_ON_CLOSE
@@ -12,37 +8,35 @@ String word
 @Bindable
 class UserInput {
     String word
-    String toString() { "$word" }
 }
 
-def userInput = new UserInput(word: '')
+def userInput = new UserInput(word: null)
 
-def swingBuilder = new SwingBuilder()
-swingBuilder.edt {
+def swingBuilder = new SwingBuilder().edt {
     lookAndFeel 'nimbus'
-    frame (
-        title: 'Input',
-        size: [350, 230],
-        show: true,
-        locationRelativeTo: null,
-        defaultCloseOperation: EXIT_ON_CLOSE ) {
+    // frame size
+    def width = 350
+    def height = 230
+    frame (title: 'Input',
+            size: [width, height],
+            show: true,
+            locationRelativeTo: null ) {
         borderLayout(vgap: 5)
-        panel(constraints:
-                BorderLayout.CENTER,
-                border: compoundBorder([emptyBorder(10), titledBorder('Input:')])
-        ) {
+        panel(constraints: BorderLayout.CENTER,
+                border: compoundBorder([emptyBorder(10), titledBorder('Input:')])) {
             tableLayout {
                 tr {
                     td { label 'Input: ' }
-                    td { textField userInput.word, id: word, columns: 20 }
-                    println """Word: $word"""
+                    td { textField id:'input', columns: 20 }
                 }
             }
         }
         panel(constraints: BorderLayout.SOUTH) {
             button text: 'Print word', actionPerformed: {
-                println """Word: $word"""
+                println """Word: ${userInput.word}"""
             }
         }
+        // Bind the text field to the bean
+        bean userInput, word: bind { input.text }
     }
 }
