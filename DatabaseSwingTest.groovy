a04a78ac29c19741d4adec6724ae6268fd9e62e7
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2015-06-28 21:43:19 -0400

    First fully functional version.

--- DatabaseSwingTest.groovy
+++ DatabaseSwingTest.groovy
@@ -3,18 +3,38 @@
  */
 import groovy.sql.Sql
 import groovy.swing.SwingBuilder
-import groovy.beans.Bindable
 import java.awt.*
 
+/**
+ * SQL queries to be called by the Swing application.
+ * @TODO: Extract queries to a separate file
+ */
+def createTestTable = """
+        START TRANSACTION;
+        DROP TABLE IF EXISTS test;
+        CREATE TABLE test (
+            id SERIAL,
+            first_name TEXT,
+            last_name TEXT,
+            phone TEXT,
+            date_of_birth DATE
+            );
+        COMMIT;"""
 
+def insertQuery = """
+        START TRANSACTION;
+        INSERT INTO test (first_name, last_name, phone, date_of_birth)
+            VALUES ( ?, ?, ?, CAST(? AS DATE) );
+        COMMIT;"""
 
 /**
- * Define possible input fields.
+ * Define input field variables.
  */
-@Bindable
 class UserInput {
-    String firstName;
-    String lastName;
+    String firstName
+    String lastName
+    String phone
+    String dateOfBirth
 }
 
 /**
@@ -22,10 +42,14 @@ class UserInput {
  */
 def userInput = new UserInput(
         firstName: null,
-        lastName: null)
+        lastName: null,
+        phone: null,
+        dateOfBirth: null)
 
+/**
+ * Swing application starts here.
+ */
 def swingBuilder = new SwingBuilder()
-
 swingBuilder.edt {
 
     // style of form
@@ -49,6 +73,7 @@ swingBuilder.edt {
                 border: compoundBorder(
                         [emptyBorder(10),
                          titledBorder('Complete the following form:')])) {
+            // input fields
             tableLayout {
                 tr {
                     td { label 'First name:' }
@@ -58,61 +83,81 @@ swingBuilder.edt {
                     td { label 'Last name:' }
                     td { textField id:'lastName', columns: 20 }
                 }
+                tr {
+                    td { label 'Phone:' }
+                    td { textField id:'phone', columns: 20 }
+                }
+                tr {
+                    td { label 'Date of birth:' }
+                    td { textField id:'dateOfBirth', columns: 20 }
+                }
             }
         }
-        // testing code only
+        /**
+         * Confirm user input by printing to console
+         * @TODO Include this in the GUI
+         */
         panel(constraints: BorderLayout.SOUTH) {
-            button text: 'Print', actionPerformed: {
-                println """You entered:"""
-                println """First name: ${userInput.firstName}"""
-                println """Last name: ${userInput.lastName}"""
+            button text: 'Submit', actionPerformed: {
+                println """
+You entered:
+First name: ${userInput.firstName}
+Last name: ${userInput.lastName}
+Phone: ${userInput.phone}
+Date of birth: ${userInput.dateOfBirth}
+                """
+
+                /**
+                 * Open DB connection, create a table, insert data.
+                 * @TODO Extract credentials to configuration file.
+                 * @TODO Extract SQL queries to a separate file
+                 */
+                def dbUrl      = 'jdbc:postgresql://localhost/GroovyTest'
+                def dbUser     = 'Phrancis'
+                def dbPassword = 'test'
+                def dbDriver   = 'org.postgresql.Driver'
+
+                def sql = Sql.newInstance(dbUrl, dbUser, dbPassword, dbDriver)
+
+                println 'Sql Instance: ' + sql
+
+                sql.execute 'SET SEARCH_PATH TO groovy_test;'
+
+                sql.execute createTestTable
+
+                def personData = [userInput.firstName, userInput.lastName, userInput.phone, userInput.dateOfBirth]
+
+                sql.execute insertQuery, personData
+
+                /**
+                 * Confirm that data was inserted successfully.
+                 * For admin purposes only, no need to add to GUI.
+                 */
+
+                sql.eachRow('''SELECT 1
+                                  id
+                                , first_name
+                                , last_name
+                                , phone
+                                , date_of_birth
+                                FROM test
+                                ORDER BY id DESC;''') { row ->
+                    println """
+New data appended to table:
+Person Id: ${row.id}
+Name: ${row.first_name} ${row.last_name}
+Phone: ${row.phone}
+Date of birth: ${row.date_of_birth}"""
+                }
+
+                sql.close()
+
             }
         }
         // Bind the fields to the bean
         bean userInput, firstName: bind { firstName.text }
         bean userInput, lastName: bind { lastName.text }
+        bean userInput, phone: bind { phone.text }
+        bean userInput, dateOfBirth: bind { dateOfBirth.text }
     }
 }
-
-
-def dbUrl      = 'jdbc:postgresql://localhost/GroovyTest'
-def dbUser     = 'Phrancis'
-def dbPassword = 'test'
-def dbDriver   = 'org.postgresql.Driver'
-
-def sql = Sql.newInstance(dbUrl, dbUser, dbPassword, dbDriver)
-
-println 'Sql Instance: ' + sql
-
-sql.execute 'SET SEARCH_PATH TO groovy_test;'
-
-sql.execute """
-        START TRANSACTION;
-        DROP TABLE IF EXISTS test;
-        CREATE TABLE test (
-            id SERIAL,
-            string TEXT,
-            num INTEGER,
-            decimal NUMERIC,
-            datetime TIMESTAMP
-            );
-        COMMIT;"""
-
-
-def sqlInsert = { List<Object> params ->
-    sql.execute """
-        START TRANSACTION;
-        INSERT INTO test (string, number, decimal, datetime)
-            VALUES (?, ?, ?, CAST(? AS TIMESTAMP));
-        COMMIT;""", params
-}
-
-sql.eachRow('SELECT * FROM test;') { row ->
-    println """
-The row Id is: ${row.id}
-The string is: ${row.string}
-The number is: ${row.number}
-The decimal is: ${row.decimal}
-The date-time is: ${row.datetime}"""
-}
-sql.close()
