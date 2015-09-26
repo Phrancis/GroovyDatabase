758303d7f46a7494d38c4d45b5824e5cd8e60105
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2015-09-26 18:25:24 -0400

    testAddUserDetail is failing

--- src/main/groovy/UserDocument.groovy
+++ src/main/groovy/UserDocument.groovy
@@ -1,21 +1,67 @@
 package groovydatabase
+
+import com.mongodb.BasicDBObjectBuilder
+import com.mongodb.DBCollection
+import groovy.transform.ToString
+
 import java.security.MessageDigest
 
+@ToString(includeNames = true, includeFields = true)
 class UserDocument {
     String userName
     String passwordHash
     Date dateCreated
-
-    UserDocument(String userName, String passwordHash, Date dateCreated) {
+    /**
+     * Constructor.
+     * TODO: Look into better hashing algorithms to use instead of MD5.
+     * @param userName  The name of the new user
+     * @param passwordHash  The password after it is hashed.
+     * @param dateCreated  The date when the user is created, defaulting to "now" but can be passed another date
+     */
+    UserDocument(String userName, String passwordHash, Date dateCreated = new Date()) {
         this.userName = userName
         this.passwordHash = MessageDigest.getInstance("MD5").digest(passwordHash.bytes).encodeHex().toString()
-        this.dateCreated = new Date()
+        this.dateCreated = dateCreated
+    }
+//    def toString = { UserDocument ->
+//        it.get().toString()
+//    }
+    /**
+     * Access the "users" MongoDB collection.
+     * @return DBCollection
+     */
+    static DBCollection usersCollection() {
+        def usersCollection = new CardshifterDB()
+                .getDB()
+                .getCollection("users")
+        usersCollection
+    }
+    /**
+     * Build a user document object.
+     * @return BasicDBObjectBuilder
+     */
+    def BasicDBObjectBuilder createUserDocument() {
+        def dboBuilder = new BasicDBObjectBuilder()
+                .start("user_name", userName)
+                .add("password_hash", passwordHash)
+                .add("date_created", dateCreated)
+        dboBuilder
     }
 
-    def createUserDocument() {
-        def userCollection = new CardshifterDB()
-            .getDB()
-            .getCollection("users")
-        userCollection
+    static void addUserDetails(userBuilder, Map details) {
+        userBuilder
+                .push("details")
+        details.each { k,v ->
+            userBuilder.add(k,v)
+        }
+        userBuilder.pop()
+    }
+    def insertUserDocument() {
+        def users = usersCollection()
+        def userDoc = createUserDocument()
+        users.insert(userDoc.get())
     }
 }
+
+// def usersCollection = usersCollection()
+// usersCollection.insert(dboBuilder.get())
--- src/test/groovy/UserDocumentTest.groovy
+++ src/test/groovy/UserDocumentTest.groovy
@@ -1,22 +1,48 @@
 package groovydatabase
 
+import com.mongodb.BasicDBObjectBuilder
 import com.mongodb.DBCollection
 import org.junit.Test
 import java.security.MessageDigest
 
 class UserDocumentTest {
-    def testUser = new UserDocument("myName", "myPassword", new Date())
-
+    private testUser = new UserDocument("myName", "myPassword", new Date())
+    def userDocBuilder = BasicDBObjectBuilder
     @Test
     void testUserDocument() {
         assert testUser.getUserName() == "myName"
         assert testUser.getPasswordHash() != "myPassword"
-        assert testUser.getPasswordHash() == MessageDigest.getInstance("MD5").digest("myPassword".bytes).encodeHex().toString()
+        assert testUser.getPasswordHash() == MessageDigest
+                .getInstance("MD5")
+                .digest("myPassword".bytes)
+                .encodeHex()
+                .toString()
         assert testUser.getDateCreated() instanceof Date
     }
     @Test
+    void testUsersCollection() {
+        def testUsersCollection = testUser.usersCollection()
+        assert testUsersCollection instanceof DBCollection
+    }
+    @Test
     void testCreateUserDocument() {
-        def testUserDoc = testUser.createUserDocument()
-        assert testUserDoc instanceof DBCollection
+        userDocBuilder = testUser.createUserDocument()
+        assert userDocBuilder instanceof BasicDBObjectBuilder
+
+    }
+    /*
+    2015-09-25 Phrancis: Currently failing.
+     */
+    @Test
+    void testAddUserDetails() {
+
+        def testMap = [
+                "hello": 1,
+                "world": 2
+        ]
+        testUser.addUserDetails(userDocBuilder, testMap)
+
+        println testUser.toString()
+
     }
 }
