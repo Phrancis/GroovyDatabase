49acb2280dc65aea6c5eb039a603517339de04dd
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2015-09-27 15:11:20 -0400

    Removed PostgresScripts and MongoDBScripts directories

--- MongoDbScripts/MongoTestWithHibernateOgm.groovy
+++
@@ -1,8 +0,0 @@
-package GroovyDatabase
-
-import com.mongodb.*
-import org.hibernate.ogm.*
-import org.hibernate.ogm.jpa.HibernateOgmPersistence
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-
